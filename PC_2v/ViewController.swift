70436dce20bb41415d0a39ba516a5a5e1c947b5f
AlexParco <alexparco16@gmail.com>
2023-05-20 14:38:05 -0500

    Initial Commit

---
+++ PC_2v/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PC_2v
+//
+//  Created by Alex Parco on 20/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
